dd6f5317ae25ccbb98fa3b529b18c82084a3b26e
Jeremy Smith <jmsmith57@hotmail.com>
2016-09-15 17:40:15 +0100

    added a twitter feed

--- app.js
+++ app.js
@@ -17,12 +17,6 @@ mongoose.Promise = bluebird;
 mongoose.connect(databaseUri);
 mongoose.plugin(beautifulUnique);
 
-// app.get('/*', function(req, res, next){ 
-//   res.setHeader('Last-Modified', (new Date()).toUTCString());
-//   next(); 
-// });
-
-
 if('test' !== environment) {
   app.use(morgan('dev'));
 }
@@ -35,8 +29,28 @@ app.use(express.static('public'));
 
 app.use('/api', routes);
 
-app.listen(port, function() {
+var server = app.listen(port, function() {
   console.log("Express is listening on port " + port);
+});
+
+var io = require("socket.io")(server);
+var Twit = require("twit");
+var twitter = new Twit({
+  consumer_key: process.env.TWITTER_CONSUMER_KEY,
+  consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+  access_token: process.env.TWITTER_ACCESS_TOKEN,
+  access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
+});
+
+var stream = twitter.stream("statuses/filter", {track: ["whale", "dolphin", "seal"]});
+io.on("connect", function(socket){
+  stream.on("tweet", function(tweet){
+    var data = {};
+    data.name = tweet.user.name;
+    data.text = tweet.text;
+    data.user_profile_image = tweet.user.profile_image_url;
+    socket.emit("tweets", data);
+  });
 });
 
 module.exports = app;
--- public/js/app.js
+++ public/js/app.js
@@ -53,9 +53,34 @@ function Router($stateProvider, $urlRouterProvider) {
     .state('about', {
       url: '/about',
       templateUrl: 'templates/about.html'
+    })
+    .state('liveSightings', {
+      url: '/liveSightings',
+      templateUrl: 'templates/liveSightings.html',
+      controller: 'LiveSightingsController as sightings'
     });
     
     $urlRouterProvider.otherwise('/');
+}
+angular
+  .module("OceanTripApp")
+  .controller("LiveSightingsController", LiveSightingsController);
+
+LiveSightingsController.$inject = ["$window"]
+function LiveSightingsController($window){
+
+  var socket = $window.io();
+
+  socket.on("connect", function(){
+    console.log("connected");
+  });
+
+  socket.on("tweets", function(tweet){
+    $('#tweets-stream').prepend('<div class="tweet"><img src="' + tweet.user_profile_image + '" class="avatar pull-left"/><div class="names"><span class="full-name">' + tweet.name + ' </span></div><div class="contents"><span class="text">' + tweet.text + '</span></div></div>');
+       
+       // console.log(tweet.text);
+  });
+
 }
 angular
   .module("OceanTripApp")
